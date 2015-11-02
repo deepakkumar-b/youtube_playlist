1eea2f809f2f85744cad8229abf63b249682404c
Deepak Kumar Balasubramaniam <deepak61289@gmail.com>
2015-11-02 00:46:21 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,77 @@
+// name our angular app
+var firstApp = angular.module('firstApp', [])
+.config(function($sceDelegateProvider) {
+        $sceDelegateProvider.resourceUrlWhitelist([
+                                                   'self',
+                                                   '*://www.youtube.com/**'
+                                                   ]);
+        });
+
+firstApp.controller('mainController', function($http, $scope) {
+
+ // bind this to vm (view-model)
+var vm = this;
+$scope.selectedURL="";
+$scope.showModal = false;
+    $scope.toggleModal = function(selectedArtist){
+                       $scope.showModal = !$scope.showModal;
+                       $scope.selectedURL = $scope.getVideo(selectedArtist.videoId);
+					   alert($scope.selectedURL);
+        };
+
+ getResults('One Direction');
+ 
+ function getResults(selectedArtist){
+                       $http.get("/"+selectedArtist).success(function(response){
+                       vm.message = response;
+					   });
+        }
+		
+ $scope.getVideo = function (id) {
+                       return "https://www.youtube.com/embed/"+id+"?rel=0"
+		}
+		
+});
+
+firstApp.directive('modal', function () {
+                  return {
+                  template: '<div class="modal fade">' +
+                  '<div class="modal-dialog" >' +
+                  '<div class="modal-content" width="800">' +
+                  '<div class="modal-header">' +
+                  '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+                  '<h4 class="modal-title">{{ title }}</h4>' +
+                  '</div>' +
+                  '<div id="player-modal" class="modal-body" ng-transclude></div>' +
+                  '</div>' +
+                  '</div>' +
+                  '</div>',
+                  restrict: 'E',
+                  transclude: true,
+                  replace:true,
+                  scope:true,
+                  link: function postLink(scope, element, attrs) {
+                  scope.title = attrs.title;
+                  
+                  scope.$watch(attrs.visible, function(value){
+                               if(value == true)
+                               $(element).modal('show');
+                               else
+                               $(element).modal('hide');
+                               });
+                  
+                  $(element).on('shown.bs.modal', function(){
+                                scope.$apply(function(){
+                                             scope.$parent[attrs.visible] = true;
+                                             });
+                                });
+                  
+                  $(element).on('hidden.bs.modal', function(){
+                                $("#player-modal iframe").attr("src", $("#player-modal iframe").attr("src"));
+                                scope.$apply(function(){
+                                             scope.$parent[attrs.visible] = false;
+                                             });
+                                });
+                  }
+                  };
+                  });
---
+++ server.js
@@ -0,0 +1,61 @@
+// load the express package and create our app
+var express = require('express'),
+	googleapis = require('googleapis'),
+	app = express(),
+	youtubeClient = null,
+	VALID_ORDER_CRITERIA = ['date', 'rating', 'relevance', 'title', 'videoCount', 'viewCount'], 
+	API_KEY = 'AIzaSyAN7FiXp7dJrN8-0uMGXQhOvE6sWDJBj2g';
+	MAX_LIMIT = 25;
+	
+var app = express();
+var path = require('path');
+var youtubeClient = null;
+
+app.use(express.static(__dirname+'/'));
+
+googleapis.options ({ auth: API_KEY });
+var youtube = googleapis.youtube ('v3');
+
+app.get('/:artist',function(req,res){
+        
+  youtube.search.list(
+        {
+         part: 'snippet',
+         q: req.params.artist,
+		 maxResults: MAX_LIMIT,
+         type: 'video',
+		 //order: 'relevance',
+         videoEmbeddable: true
+        },
+        function(err,result){
+                if(err)
+                        console.log(err);
+                else{
+						var results = [];
+                        for(var i=0;i<MAX_LIMIT;i++) {
+                            var item = result.items[i];
+                            var resultArray= { };
+							resultArray.videoId = item.id.videoId;
+                            resultArray.title = item.snippet.title;
+							resultArray.thumbnail = item.snippet.thumbnails.default;
+                            results.push(resultArray);
+                        }
+                       
+                        console.log(JSON.stringify(results,null,2));
+                        res.setHeader('Content-Type', 'application/json');
+                        res.send(JSON.stringify(results,null,2));
+						
+                }
+
+        });
+});
+
+ // send our index.html file to the user for the home page
+app.get('/', function(req, res) {
+res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+// start the server
+app.listen(1337);
+console.log('1337 is the magic port!');
+ 
